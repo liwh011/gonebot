5b7ca3d6ba6568da4d1721677d0ef520a3535fdd
liwh011 <304180208@qq.com>
2022-04-13 15:39:50 +0800

    修改部分结构体名；修改插件的实现方式，改为接口，使用manager管理，同时限制插件访问权力

--- plugin.go
+++ plugin.go
@@ -5,100 +5,144 @@ import (
 	"reflect"
 )
 
+func init() {
+	defaultPluginManager = newPluginManager()
+	// 添加钩子，Engine创建完毕后，初始化插件
+	EngineHookManager.OnCreated(defaultPluginManager.InitPlugins)
+}
+
+// TODO 运行时装卸
+
+// 插件管理器，理应是个单例
+type pluginManager struct {
+	plugins             map[string]Plugin
+	pluginConfigStructs map[string]interface{}
+}
+
+var defaultPluginManager *pluginManager
+
+func newPluginManager() *pluginManager {
+	pm := &pluginManager{
+		plugins:             make(map[string]Plugin),
+		pluginConfigStructs: make(map[string]interface{}),
+	}
+
+	return pm
+}
+
+func (pm *pluginManager) RegisterPlugin(plugin Plugin, pluginConfig interface{}) {
+	if plugin == nil {
+		panic("插件不能为空")
+	}
+	id := getPluginId(plugin)
+	pm.plugins[id] = plugin
+
+	if pluginConfig != nil {
+		pm.pluginConfigStructs[id] = pluginConfig
+	}
+}
+
+// 初始化插件
+func (pm *pluginManager) InitPlugins(engine *Engine) {
+	pluginControlConfig := engine.Config.Plugin.Enable
+
+	for id, plugin := range pm.plugins {
+		// 仅当配置中指定为禁用的插件才不加载。配置中未指定的插件默认启用
+		if enabled, ok := pluginControlConfig[id]; ok && !enabled {
+			continue
+		}
+
+		// 填充插件配置结构体字段
+		if plgCfgStruct, ok := pm.pluginConfigStructs[id]; ok {
+			convertConfigMapToStruct(plgCfgStruct, engine.Config.Plugin.Config[id])
+		}
+
+		proxy := newEngineProxy(engine)
+		plugin.Init(&proxy)
+	}
+}
+
+func (pm *pluginManager) GetPluginById(id string) Plugin {
+	return pm.plugins[id]
+}
+
+func (pm *pluginManager) GetPlugin(name, author string) Plugin {
+	id := fmt.Sprintf("%s@%s", name, author)
+	return pm.plugins[id]
+}
+
+func (pm *pluginManager) GetPluginConfig(plugin Plugin) interface{} {
+	return pm.pluginConfigStructs[getPluginId(plugin)]
+}
+
+// 注册插件。插件配置结构体pluginConfig可选，使用反射映射到字段，无则传nil
+func RegisterPlugin(plugin Plugin, pluginConfig interface{}) {
+	defaultPluginManager.RegisterPlugin(plugin, pluginConfig)
+}
+
+// 获取插件配置
+func GetPluginConfig(plugin Plugin) interface{} {
+	return defaultPluginManager.GetPluginConfig(plugin)
+}
+
+// 插件信息，其中Name和Author共同唯一标识一个插件
 type PluginInfo struct {
 	Name        string
-	Description string
-	Version     string
 	Author      string
+	Version     string
+	Description string
 }
 
-type Plugin struct {
-	Handler
-	id     string
-	engine *Engine // 插件所属的engine
-	Info   PluginInfo
-	Config interface{} // 结构体指针
-	onInit func(*Plugin)
+type Plugin interface {
+	Init(engine *EngineProxy) // 初始化插件
+	GetPluginInfo() PluginInfo
 }
 
-func (p *Plugin) GetEngine() *Engine {
-	return p.engine
+// 获取插件的唯一标识，格式为：“插件名@作者”
+func getPluginId(plugin Plugin) string {
+	return fmt.Sprintf("%s@%s", plugin.GetPluginInfo().Name, plugin.GetPluginInfo().Author)
 }
 
-// 添加在某个Engine上
-func (p *Plugin) AttachTo(engine *Engine) {
-	p.engine = engine
-	engine.addSubHandler(&p.Handler, EventNameAllEvent)
+// Engine对象的代理，仅提供有限的访问。
+type EngineProxy struct {
+	engine        *Engine
+	pluginHandler *Handler
 }
 
-// 卸载插件
-func (p *Plugin) Unload() {
-	p.engine.removeSubHandler(&p.Handler, EventNameAllEvent)
+func newEngineProxy(engine *Engine) EngineProxy {
+	ret := EngineProxy{engine: engine}
+	ret.pluginHandler, _ = engine.NewRemovableHandler()
+	return ret
 }
 
-var plugins = make(map[string]*Plugin)
-
-// 注册一个新插件。
-//
-// info: 插件信息，包括名称、描述、版本、作者
-//
-// cfg: 插件配置，传入结构体指针，如果无需配置可以为 nil
-//
-// onInit: 插件初始化时的回调函数，如果无需初始化可以为 nil
-func NewPlugin(info PluginInfo, cfg interface{}, onInit func(*Plugin)) *Plugin {
-	np := &Plugin{
-		Handler: Handler{
-			subHandlers: make(map[EventName][]*Handler),
-		},
-		id:     fmt.Sprintf("%s@%s", info.Name, info.Author),
-		Info:   info,
-		Config: cfg,
-		onInit: onInit,
-	}
-
-	if p, ok := plugins[np.id]; ok {
-		panic(fmt.Errorf("插件 %s 已经注册。该插件为 %v", np.id, p.Info))
-	}
-
-	plugins[np.id] = np
-	return np
+// 新建一个Handler，用于处理指定类型的事件，不写则处理所有类型的事件
+func (p *EngineProxy) NewHandler(eventTypes ...EventName) *Handler {
+	return p.pluginHandler.NewHandler(eventTypes...)
 }
 
-func GetPlugin(id string) *Plugin {
-	return plugins[id]
+func (p *EngineProxy) GetEngineConfig() *BaseConfig {
+	return p.engine.Config
 }
 
-func LoadPlugins(engine *Engine) {
-	cfg := engine.Config
-
-	// 默认加载每一个插件。如果配置中指定了某插件的启用状态，则按配置的来。
-	for id, plugin := range plugins {
-		if enable, ok := cfg.Plugin.Enable[id]; ok && enable || !ok {
-			plugin.AttachTo(engine)
-			plugin.convertMapToConfig(cfg.Plugin.Config[id])
-			plugin.onInit(plugin)
-		}
-	}
-}
-
-func (target *Plugin) convertMapToConfig(src PluginConfig) {
-	if target == nil {
+func convertConfigMapToStruct(cfgStruct interface{}, srcMap PluginConfigMap) {
+	if srcMap == nil {
 		return
 	}
 
-	if src == nil {
-		return
+	if cfgStruct == nil {
+		panic("cfgStruct不能为空")
 	}
 
-	cfg := &target.Config
-	value := reflect.ValueOf(cfg).Elem().Elem()
+	// cfg := &target.Config
+	value := reflect.ValueOf(cfgStruct)
+
 	if value.Kind() != reflect.Ptr {
-		panic(fmt.Errorf("插件配置必须是结构体指针类型，而不是 %T(%v)", target.Config, value.Kind()))
+		panic(fmt.Errorf("必须传入结构体指针，而不是 %T (%v)", cfgStruct, value.Kind()))
 	}
 	value = value.Elem()
 	if value.Kind() != reflect.Struct {
-		panic(fmt.Errorf("插件配置必须是结构体指针类型，而不是 %v", value.Kind()))
+		panic(fmt.Errorf("必须传入结构体指针，而不是 %v", value.Kind()))
 	}
 
-	mapToStruct(src, value.Addr().Interface())
+	mapToStruct(srcMap, value.Addr().Interface())
 }
